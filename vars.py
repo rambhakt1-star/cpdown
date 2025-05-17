c692ae2e0724fcf601fe937534cd314bdfe8d97e
rambhakt1-star <rambhakthoonme@gmail.com>
2025-05-17 19:47:12 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,9 @@
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6948815452:AAHLdIf8gKLDp_MMkt8iSJ6wLOTdsAsIq-I"
-auth_users = [1713924419]
-sudo_user = 1713924419
-log_channel = -1002143629860
-txt_channel = -1002049391187
+api_id = 
+api_hash = ""
+bot_token = ""
+auth_users = [6890400066]
+sudo_user = 6890400066
+log_channel = -1002622118184
+txt_channel = -1002561868621
 
 
